b26d4981fd1bbabb80b27764c6242902606ad3b8
Walter Yu <kharma@gmail.com>
2011-11-26 19:17:30 -0800

    Add Heroku Redis config block.

--- blurb.rb
+++ blurb.rb
@@ -5,8 +5,7 @@ redis = Redis.new
 
 configure do
   require 'redis'
-  redisUri = ENV["REDISTOGO_URL"] || 'redis://localhost:6379'
-  uri = URI.parse(redisUri) 
+  uri = URI.parse(ENV["REDISTOGO_URL"])
   REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 end
 
