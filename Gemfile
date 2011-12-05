bdf12c4dcd309a95881ef16d087879824736aaf1
Dave Doolin <david.doolin@gmail.com>
2011-12-05 11:24:53 -0800

    Start of rspec framework.

--- Gemfile
+++ Gemfile
@@ -5,3 +5,7 @@ gem 'redis'
 gem 'thin'
 gem 'sass'
 gem 'rb-fsevent'
+
+group :test do
+  gem 'rspec'
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,5 @@
+
+RSpec.configure do |config|
+  config.mock_with :rspec
+  #config.fixture_path = "#{::Rails.root}/spec/fixtures"
+end
