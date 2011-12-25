2f1e3626d308a705d74ca6b1e5e0c07506781cac
Dave Doolin <david.doolin@gmail.com>
2011-12-24 16:31:28 -0800

    Function for selecting text in whole result div.

--- public/javascript/copy.js
+++ public/javascript/copy.js
@@ -17,10 +17,34 @@ jQuery.fn.selText = function() {
     return this;
 }
 
+
+function selectElementText(el, win) {
+    win = win || window;
+    var doc = win.document, sel, range;
+    if (win.getSelection && doc.createRange) {
+        sel = win.getSelection();
+        range = doc.createRange();
+        range.selectNodeContents(el);
+        sel.removeAllRanges();
+        sel.addRange(range);
+    } else if (doc.body.createTextRange) {
+        range = doc.body.createTextRange();
+        range.moveToElementText(el);
+        range.select();
+    }
+}
+
+  $(function () {
+        $("#result").click(function () {
+             selectElementText(document.getElementById("result"));
+        })
+  });
+
 $(document).ready(function() {
 
-  $('.result').click(function() {
+/*  $('.result').click(function() {
     $(this).selText().addClass("selected");
   });
+  */
 
 });
