72576eb9eb406c2d10eee8e16c508865df60fd20
Dave Doolin <david.doolin@gmail.com>
2011-12-24 16:32:21 -0800

    Removed non-working functions.

--- public/javascript/copy.js
+++ public/javascript/copy.js
@@ -1,22 +1,3 @@
-jQuery.fn.selText = function() {
-    var obj = this[0];
-    if ($.browser.msie) {
-        var range = obj.offsetParent.createTextRange();
-        range.moveToElementText(obj);
-        range.select();
-    } else if ($.browser.mozilla || $.browser.opera) {
-        var selection = obj.ownerDocument.defaultView.getSelection();
-        var range = obj.ownerDocument.createRange();
-        range.selectNodeContents(obj);
-        selection.removeAllRanges();
-        selection.addRange(range);
-    } else if ($.browser.safari) {
-        var selection = obj.ownerDocument.defaultView.getSelection();
-        selection.setBaseAndExtent(obj, 0, obj, 1);
-    }
-    return this;
-}
-
 
 function selectElementText(el, win) {
     win = win || window;
@@ -34,17 +15,8 @@ function selectElementText(el, win) {
     }
 }
 
-  $(function () {
-        $("#result").click(function () {
+$(function () {
+    $("#result").click(function () {
              selectElementText(document.getElementById("result"));
-        })
-  });
-
-$(document).ready(function() {
-
-/*  $('.result').click(function() {
-    $(this).selText().addClass("selected");
-  });
-  */
-
+    })
 });
